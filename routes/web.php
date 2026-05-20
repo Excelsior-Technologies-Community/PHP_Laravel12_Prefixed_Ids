eec77a78848809b907433fa52fa1be9d61a905e7
patelmanasi <manasipatel0852@gmail.com>
2026-05-20 11:51:00 +0530

    feat(prefixed-ids): implement modern dark-mode orders dashboard with prefixed ID management, search, pagination, analytics, glassmorphism UI, and delete functionality

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrderController;
 
+Route::get('/', function () {
+    return redirect('/orders');
+});
+
 Route::get('/orders', [OrderController::class, 'index'])->name('orders.index');
+
 Route::get('/orders/create', [OrderController::class, 'create'])->name('orders.create');
+
 Route::post('/orders/store', [OrderController::class, 'store'])->name('orders.store');
+
 Route::get('/orders/{order}', [OrderController::class, 'show'])->name('orders.show');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::delete('/orders/{order}', [OrderController::class, 'destroy'])->name('orders.destroy');
